49f67c2b4d7e4b4d76fc1c4bdaf2751b95dc6c2c
KTScompany <141685007+KTScompany@users.noreply.github.com>
2025-11-23 13:20:57 -0500

    Add Firebase configuration to config.js
    Added Firebase configuration details.

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,1 +1,10 @@
 // Firebase config placeholder
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD_aJGoW0nCngxGwm3WcGvrPVCpLASTdds",
+  authDomain: "keystone-tactical-supply.firebaseapp.com",
+  projectId: "keystone-tactical-supply",
+  storageBucket: "keystone-tactical-supply.firebasestorage.app",
+  messagingSenderId: "323627708038",
+  appId: "1:323627708038:web:9a48b648591304a52feb07"
+};
